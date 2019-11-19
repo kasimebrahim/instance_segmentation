12436c1f71a347a6ce75e600f78d3f6d5965628b
kasimebrahim <se.kasim.ebrahim@gmail.com>
2019-11-19 13:20:45 +0300

    Infer and generate json

--- Segmentation.py
+++ Segmentation.py
@@ -37,6 +37,12 @@ class SegmentationConfig(Config):
     # Weight decay regularization
     # WEIGHT_DECAY = 0.01
 
+class InferenceConfig(SegmentationConfig):
+    GPU_COUNT = 1
+    IMAGES_PER_GPU=1
+    BACKBONE="resnet50"
+    # BACKBONE="resnet101"
+
 def train(model):
     datasets_training = ld.SegmentationDataSet()
     datasets_validation = ld.SegmentationDataSet()
@@ -53,6 +59,60 @@ def train(model):
 
     # model.train(datasets_training, datasets_validation, config.LEARNING_RATE,
     #             epochs=100, layers='heads')
+
+def infer(model, data_dir):
+    SEGMENTS = ["BG", "title", "subtitle", "paragraph", "footnotes",
+                "header", "footer", "page", "signature"]
+
+    from collections import defaultdict
+    documents = defaultdict(list)
+    for doc, page, path in gen_image_path(data_dir):
+        print("\nDetecting : {}\n".format(path))
+        image = skimage.io.imread(path)
+        results = model.detect([image], verbose=1)[0]
+        page = {'page':page, 'elements':[]}
+
+        ###################
+        for c, b in zip(results['class_ids'], results['rois']):
+            page['elements'].append({
+                    'type': SEGMENTS[c],
+                    'y1':str(b[0]),
+                    'x1':str(b[1]),
+                    'y2':str(b[2]),
+                    'x2':str(b[3])})
+
+        documents[doc].append(page)
+    # write to file
+    with open('documents.json', 'w') as out:
+        json.dump(documents, out, sort_keys=True)
+
+########################################################
+# generate document_name, page, path of images from
+# data_set directory. supports two types of date set
+# arrengements.
+#
+# One: Each documenat is in its own directory and every
+#      page of the document is in the directory.
+#      i:e ds/doc/p01.jpg -> (doc, p01, datasets/doc/p01.jpg)
+# Two: All pages of all the documents are under one
+#      directory. And every page is named as document_name
+#      concatinated with page name/number.
+#      i:e ds/doc_p01.jpg -> (doc, p01, ds/doc_p01.jpg)
+########################################################
+def gen_image_path(parent_dir):
+    path, dirs, files = next(os.walk(parent_dir))
+    # if wrapped by a directory then the dir_name is the
+    # document name and the file name is the page name.
+    for dir in dirs:
+        new_path, _,_files = next(os.walk(os.path.join(path, dir)))
+        for file in _files:
+            # document, page, path_to_image
+            yield (dir, file.rsplit(".", 1)[0], os.path.join(new_path, file))
+    # if not wrapped by a directory then split the file_name
+    # to document name and file name.
+    for file in files:
+        doc, page = file.rsplit(".", 1)[0].rsplit("_",1)
+        yield (doc, page, os.path.join(path, file))
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
